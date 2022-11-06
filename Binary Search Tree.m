5d89bd6dbe3d78945c64fcb0b9dd29e1a04e866e
tahirpinarli <74795594+tahirpinarli@users.noreply.github.com>
2022-11-06 17:12:31 +0200

    Create Binary Search Tree.m

---
+++ Binary Search Tree.m
@@ -0,0 +1,12 @@
+# Kodluyoruz-Veri-Yap-lar-ve-Algoritmalar
+A. [7, 5, 1, 8, 3, 6, 0, 9, 4, 2] dizisinin Binary-Search-Tree aşamalarını yazınız.
+  
+ ** Root değerini 7 olarak seçtik.
+         7
+      5     8
+    1   6 
+  0   3
+     2 4   
+  Bu sorting (sıralama) yönteminde, önce bir root değeri belirlenir. 
+  Daha sonra sırayla, dizideki elemanlar root değerinden küçük veya büyük olmalarına göre sıralamaya (tree) eklenerek sorting işlemine devam edilir. 
+  Kıyaslanan değerler root değerinden küçükse sol, büyükse sağ tarafa yazılır. Tüm elemanlara aynı işlem uygulandığında, sıralama işlemi bitmiş olur.
